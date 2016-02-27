6c9926422ff5b0b29b9d63391515931f32d7a5ed
George Cook <george.cook@twintechs.com>
2016-02-27 15:39:19 -0500

    added ability to fix the namespace (not using proper refactoring; but better than nothing).

---
+++ EditorComfortAddin/TwinTechs/EditorExtensions/Commands/FixNamespace.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using MonoDevelop.Components.Commands;
+using MonoDevelop.Ide;
+using TwinTechs.EditorExtensions.Helpers;
+using ICSharpCode.NRefactory.TypeSystem;
+using ICSharpCode.NRefactory.CSharp.TypeSystem;
+using System.Text.RegularExpressions;
+using System.IO;
+using MonoDevelop.Projects;
+
+namespace TwinTechs.EditorExtensions.Commands
+{
+	public class FixNamespace: CommandHandler
+	{
+		protected override void Run ()
+		{
+			var document = IdeApp.Workbench.ActiveDocument;
+			var editor = document.Editor;
+			var project = document.Project as DotNetProject;
+			if (project != null) {
+				var nameSpaceText = project.GetDefaultNamespace (document.FileName.ToString ());
+
+				var namespaceLine = editor.GetLine (editor.Caret.Line);
+				var oldNamespacetext = editor.GetLineText (editor.Caret.Line);
+				var oldNamespace = GetNameSpaceAtCaret ();
+				var newNamespaceText = oldNamespacetext.Replace (oldNamespace, nameSpaceText);
+				editor.Replace (namespaceLine.Offset, namespaceLine.Length, newNamespaceText);
+			}
+		}
+
+		protected override void Update (CommandInfo info)
+		{
+			var isEnabled = IdeApp.Workspace.GetIsWorkspaceOpen () && IdeApp.Workspace.GetIsDocumentOpen ()
+			                && GetNameSpaceAtCaret () != null;
+
+			info.Enabled = isEnabled;
+			info.Visible = true;
+		}
+
+		string GetNameSpaceAtCaret ()
+		{
+			var document = IdeApp.Workbench.ActiveDocument;
+			var editor = document.Editor;
+
+			var pattern = @"(?<=(namespace))[^_]([^{]*)";
+			var nameSpaceMatch = Regex.Match (editor.GetLineText (editor.Caret.Line), pattern);
+			if (nameSpaceMatch.Success) {
+				return nameSpaceMatch.ToString ().Trim ();
+			} else {
+				return null;
+			}
+		}
+	}
+}
+
---
+++ EditorComfortAddin/TwinTechs/EditorExtensions/Helpers/DocumentHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using MonoDevelop.Ide;
+
+namespace TwinTechs.EditorExtensions.Helpers
+{
+	public class DocumentHelper
+	{
+		public DocumentHelper ()
+		{
+		}
+
+		public static string GetRootFileNameForActiveDocument ()
+		{
+			var project = IdeApp.Workbench.ActiveDocument.Project;
+			var filename = IdeApp.Workbench.ActiveDocument.FileName;
+			var filePath = filename.FullPath.ToString ();
+			if (filePath?.Length > 0) {
+				var projectPath = project.GetAbsoluteChildPath ("").ToString ();
+				if (projectPath?.Length < filePath.Length) {
+					var relativeFilePath = filename.ToString ().Substring (projectPath.Length);
+					return relativeFilePath;
+				} else {
+					return null;
+				}
+			} else {
+				return null;
+
+			}
+		}
+	}
+}
+

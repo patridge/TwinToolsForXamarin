215ef884699476a3b37a3ea2b5bb52ba1998c4e5
George Cook <george.cook@twintechs.com>
2016-02-29 17:16:33 -0500

    version bounce

--- EditorComfortAddin/Properties/AssemblyInfo.cs
+++ EditorComfortAddin/Properties/AssemblyInfo.cs
@@ -8,13 +8,13 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
 [assembly: AssemblyProduct ("")]
-[assembly: AssemblyCopyright ("2015 George James Edward Cook")]
+[assembly: AssemblyCopyright ("2016 George James Edward Cook")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion ("1.0.1")]
+[assembly: AssemblyVersion ("1.2")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
